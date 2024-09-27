e0cddaa9f9434b3127494b6e6e3bc55baff5eda5
Admin <Chavdamahesh2772@gmail.com>
2024-09-27 08:45:30 +0530

    Utility function added

---
+++ backend/common/utilityFunction.js
@@ -0,0 +1,26 @@
+const linkPreviewGenerator = require('link-preview-generator');
+const createError = require('http-errors')
+
+// Fetch URL Details
+module.exports.fetchUrlDetails = async (url) => {
+    return new Promise(async function (resolve, reject) {
+        // validate
+        if (!url) 
+            return reject(createError('URL cannot blank'));
+
+        // Checking for actual URL
+        try {
+            new URL(url);
+        } catch (error) {
+            return reject(createError('Invalide URL'));
+        }
+
+        // Fetch Details By Link Preview
+        try {
+            let data = await linkPreviewGenerator(url)
+            resolve(data);
+        } catch (error) {
+            reject({ error: 'Failed to fetch preview data' });
+        }
+    });
+};
